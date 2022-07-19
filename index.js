96a19bc328b3e1408bcf7ebfe9f0f6f9d619e742
TamazT <108069650+TamazT@users.noreply.github.com>
2022-07-19 19:38:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-import { ethers } from "./ethers-5.6.esm.min.js"
-import { abi, contractAddress } from "./constants.js"
-
 const connectButton = document.getElementById("connectButton")
 connectButton.onclick = connect
 function connect() {
