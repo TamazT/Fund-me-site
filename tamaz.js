f9a82761abf20dc92fe4a2980af0dfe54a7c74ff
TamazT <108069650+TamazT@users.noreply.github.com>
2022-07-20 09:39:47 +0300

    Delete tamaz.js

--- tamaz.js
+++
@@ -1,5 +0,0 @@
-const connectButton = document.getElementById("connectButton")
-connectButton.onclick = connect
-function connect() {
-    window.ethereum.request({method: "eth_requestAccounts"})
-}
